0c432e731f8d3b3f369542b3dd812f6d045c91b8
Md. Mamun Or Rashid (Rajon) <rajonhossaindhaka@gmail.com>
2026-04-21 22:58:48 +0600

    first

--- lib/main.dart
+++ lib/main.dart
@@ -227,7 +227,6 @@ class _LoginScreenState extends State<LoginScreen> {
                           ),
                         ],
                       ),
-
                       const Spacer(),
                     ],
                   ),
